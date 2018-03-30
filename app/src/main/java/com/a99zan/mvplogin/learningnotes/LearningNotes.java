0a08a957549878deba9e7b83b66bf1b8f2c742b4
weidong <466463054@qq.com>
2018-03-30 16:53:09 +0800

    1

---
+++ app/src/main/java/com/a99zan/mvplogin/learningnotes/LearningNotes.java
@@ -0,0 +1,11 @@
+package com.a99zan.mvplogin.learningnotes;
+
+/**
+ * Created by 99zan on 2018/3/30.
+ */
+
+public class LearningNotes {
+    //2018.03.30
+    //初步熟悉MVP架构，写了一个MVP架构的Demo
+    //初步熟悉Dagger2依赖注入框架
+}
