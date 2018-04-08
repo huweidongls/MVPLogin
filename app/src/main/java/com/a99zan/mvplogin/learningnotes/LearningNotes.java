a2939f7874dc7a7da7177c4702f831000f1da45d
weidong <466463054@qq.com>
2018-04-08 14:50:45 +0800

    1

--- app/src/main/java/com/a99zan/mvplogin/learningnotes/LearningNotes.java
+++ app/src/main/java/com/a99zan/mvplogin/learningnotes/LearningNotes.java
@@ -16,4 +16,8 @@ public class LearningNotes {
     //2018.04.03
     //熟悉intentservice的用法，主要用于后台下载，下载完毕后自动停止
     //学习php面向对象
+
+    //2018.04.08
+    //修改好github代码仓库
+    //学习第一行代码（第二版），巩固Android基础知识，整理脑中Android思路，缺查补漏
 }
